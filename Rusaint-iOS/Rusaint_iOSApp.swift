11282dc036cc5f5d0858399e162c865701a4d3a4
LeeJoEun-01 <joeun1005@naver.com>
2024-12-15 23:09:50 +0900

    [setting] 프로젝트 및 gitignore 생성

---
+++ Rusaint-iOS/Rusaint_iOSApp.swift
@@ -0,0 +1,20 @@
+//
+//  Rusaint_iOSApp.swift
+//  Rusaint-iOS
+//
+//  Created by 이조은 on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Rusaint_iOSApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
