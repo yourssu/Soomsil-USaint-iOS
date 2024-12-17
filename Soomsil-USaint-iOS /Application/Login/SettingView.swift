268c99de8b4b30d43d73182c92c06827bd5eb655
LeeJoEun-01 <joeun1005@naver.com>
2024-12-18 03:27:04 +0900

    [feat] HomeView 수정 및 ReportCard CoreData 연결

---
+++ Soomsil-USaint-iOS /Application/Login/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Soomsil-USaint-iOS 
+//
+//  Created by 이조은 on 12/18/24.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
