6cdcd35a0e8c058f9291388ee3922c6fa8f28a3d
LeeJoEun-01 <joeun1005@naver.com>
2024-12-18 12:12:23 +0900

    [#8] SettingView 제작

--- Soomsil-USaint-iOS /Application/Login/SettingView.swift
+++ Soomsil-USaint-iOS /Application/Login/SettingView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import YDS_SwiftUI
 
 struct SettingView: View {
     @Environment(\.dismiss) var dismiss
@@ -13,7 +14,28 @@ struct SettingView: View {
     @Binding var path: [StackView]
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text("설정")
+                .font(YDSFont.title2)
+                .padding(.top, 8)
+                .padding(.leading, 16)
+            YDSList(hasSubHeader: true, subHeaderText: "계정관리", items: [YDSListItem(text: "로그아웃")])
+            YDSList(hasSubHeader: true, subHeaderText: "알림", items: [YDSListItem(text: "알림 받기", toggle: true)])
+            YDSList(hasSubHeader: true, subHeaderText: "약관", items: [YDSListItem(text: "이용약관"), YDSListItem(text: "개인정보수집 및 허용")])
+            Spacer()
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+               ToolbarItem(placement: .topBarLeading) {
+                   Button {
+                       dismiss()
+                   } label: {
+                       Image("ic_arrow_left_line")
+                           .resizable()
+                           .frame(width: 24, height: 24)
+                   }
+               }
+           }
     }
 }
 
