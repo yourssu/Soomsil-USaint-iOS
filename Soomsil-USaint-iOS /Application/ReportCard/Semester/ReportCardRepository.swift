570fcbeb7875f277350479a8f17de2224955aabb
CJiu01 <jiwoo020801@gmail.com>
2024-12-19 16:18:43 +0900

    [#12] semester 로컬데이터 get/update/delete 함수 repository에 추가

--- Soomsil-USaint-iOS /Application/ReportCard/Semester/ReportCardRepository.swift
+++ Soomsil-USaint-iOS /Application/ReportCard/Semester/ReportCardRepository.swift
@@ -10,28 +10,85 @@ import CoreData
 import Rusaint
 
 
-/**
- Rusaint에서 반환하는 오류
- 
- 현재 오류에 대한 세부정보를 연관 값으로 받지 않고 에러 타입만 정의해둔 상태
- */
-public enum RusaintError: Error {
-    case webDynproError
-    case invalidClientError
-    case ssoLoginError
-    case applicationError
-}
-
 class ReportCardRepository {
     static let shared = ReportCardRepository(coreDataStack: .shared)
     
-    let coreDataStack: CoreDataStack
+    private let coreDataStack: CoreDataStack
+    // FIXME: - user session 위해 필요
+//    private let keychain = Keychain(service: "com.yourssu.soomsil-ios")
     
     init(coreDataStack: CoreDataStack = .shared) {
         self.coreDataStack = coreDataStack
     }
     
     // MARK: - SemesterList (Core Data)
-//    public func getSemesterList
+    private func getSemesterList() -> [GradeSummaryModel] {
+        let context = coreDataStack.taskContext()
+        let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
+        do {
+            let list = try context.fetch(fetchRequest)
+            return list.toGradeSummaryModel()
+        } catch {
+            print(error.localizedDescription)
+            return []
+        }
+    }
+    
+    private func updateSemesterList(_ rusaintSemesterList: [GradeSummaryModel]) {
+        deleteSemesterList()
+        let context = coreDataStack.taskContext()
+        for semesterList in rusaintSemesterList {
+            createSemester(year: semesterList.year,
+                           semester: semesterList.semester,
+                           gpa: semesterList.gpa,
+                           earnedCredit: semesterList.earnedCredit,
+                           semesterRank: semesterList.semesterRank,
+                           semesterStudentCount: semesterList.semesterStudentCount,
+                           overallRank: semesterList.overallRank,
+                           overallStudentCount: semesterList.overallStudentCount,
+                           in: context)
+        }
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch {
+                print("update report summary error : \(error)")
+            }
+        }
+    }
+    
+    private func deleteSemesterList() {
+        let context = coreDataStack.taskContext()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: CDSemester.fetchRequest())
+        do {
+            try context.execute(deleteRequest)
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func createSemester(
+        year: Int,
+        semester: String,
+        gpa: Float,
+        earnedCredit: Float,
+        semesterRank: Int,
+        semesterStudentCount: Int,
+        overallRank: Int,
+        overallStudentCount: Int,
+        in context: NSManagedObjectContext
+    ) {
+        let semesterEntity = CDSemester(context: context)
+        
+        semesterEntity.year = Int16(year)
+        semesterEntity.semester = semester
+        semesterEntity.gpa = gpa
+        semesterEntity.earnedCredit = earnedCredit
+        semesterEntity.semesterRank = Int16(semesterRank)
+        semesterEntity.semesterStudentCount = Int16(semesterStudentCount)
+        semesterEntity.overallRank = Int16(overallRank)
+        semesterEntity.overallStudentCount = Int16(overallStudentCount)
+    }
 }
 
