34432efaa7170f13525b2294ce6711530df9b1fd
CJiu01 <jiwoo020801@gmail.com>
2024-12-19 18:28:41 +0900

    [#12] DefaultSemesterListViewModel protocol채택및함수구현

--- Soomsil-USaint-iOS /Application/ReportCard/Semester/SemesterListViewModel.swift
+++ Soomsil-USaint-iOS /Application/ReportCard/Semester/SemesterListViewModel.swift
@@ -24,87 +24,111 @@ protocol SemesterListViewModel: BaseViewModel, ObservableObject {
     var reportList: [GradeSummaryModel] { get set }
     var isOnSeasonalSemester: Bool { get set }
     
-    func getReportList() async -> Result<[GradeSummaryModel], RusaintError>
-    func getReportListFromRusaint() async -> Result<[GradeSummaryModel], RusaintError>
+    // FIXME: - session 인자 제거
+    func getSemesterList(session: USaintSession?) async -> Result<[GradeSummaryModel], RusaintError>
+    func getSemesterListFromRusaint(session: USaintSession) async -> Result<[GradeSummaryModel], RusaintError>
 }
 
-//final class DefaultSemesterListViewModel: BaseViewModel, SemesterListViewModel {
+final class DefaultSemesterListViewModel: BaseViewModel, SemesterListViewModel {
+    @Published var reportList = [GradeSummaryModel]()
+    @Published var isOnSeasonalSemester = false
+    private let reportCardRepository = ReportCardRepository.shared
+    
+    @MainActor
+    public func getSemesterList(session: USaintSession?) async -> Result<[GradeSummaryModel], RusaintError> {
+        reportCardRepository.deleteSemesterList()
+        let gradeSummaryFromDevice = reportCardRepository.getSemesterList()
+        if !gradeSummaryFromDevice.isEmpty {
+            print("🏳️‍🌈coredata: \(gradeSummaryFromDevice)")
+            return .success(gradeSummaryFromDevice)
+        }
+        return await getSemesterListFromRusaint(session: session!)
+    }
+    
+    @MainActor
+    public func getSemesterListFromRusaint(session: USaintSession) async -> Result<[GradeSummaryModel], RusaintError> {
+        do {
+            let response = try await CourseGradesApplicationBuilder().build(session: session).semesters(courseType: CourseType.bachelor)
+            let rusaintData = response.toGradeSummaryModels()
+
+            self.reportCardRepository.updateSemesterList(rusaintData)
+            let list = self.reportCardRepository.getSemesterList()
+            if list.isEmpty {
+                throw ParsingError.error("데이터 에러")
+            } else {
+                print("🏳️‍🌈Rusaint: \(list)")
+                return .success(list)
+            }
+
+            
+        } catch (let error) {
+            return .failure(error as! RusaintError)
+        }
+    }
+
+}
+
+//final class TestSemesterListViewModel: BaseViewModel, SemesterListViewModel {
 //    @Published var reportList = [GradeSummaryModel]()
-//    @Published var isOnSeasonalSemester = false
+//    @Published var isLoading: Bool = false
+//    @Published var isOnSeasonalSemester: Bool = false
 //    private let reportCardRepository = ReportCardRepository.shared
-//    
-//    @MainActor
-//    public func getReportList() async -> Result<[GradeSummaryModel], RusaintError> {
-//        
+//
+//    func getSemesterList() async -> Result<[GradeSummaryModel], RusaintError> {
+//        return await getSemesterListFromRusaint()
 //    }
-//    
-//    @MainActor
-//    public func getReportListFromRusaint() async -> Result<[GradeSummaryModel], RusaintError> {
-//        
+//    func getSemesterListFromRusaint() async -> Result<[GradeSummaryModel], RusaintError> {
+//        return .success([
+//            GradeSummaryModel(
+//                year: 2022,
+//                semester: "1 학기",
+//                gpa: 2.22,
+//                earnedCredit: 13.5,
+//                semesterRank: 12,
+//                semesterStudentCount: 34,
+//                overallRank: 100,
+//                overallStudentCount: 566
+//            ),
+//            GradeSummaryModel(
+//                year: 2022,
+//                semester: "여름학기",
+//                gpa: 3.22,
+//                earnedCredit: 13.5,
+//                semesterRank: 123,
+//                semesterStudentCount: 111,
+//                overallRank: 1,
+//                overallStudentCount: 324
+//            ),
+//            GradeSummaryModel(
+//                year: 2022,
+//                semester: "2 학기",
+//                gpa: 4.42,
+//                earnedCredit: 4.0,
+//                semesterRank: 123,
+//                semesterStudentCount: 100,
+//                overallRank: 11,
+//                overallStudentCount: 324
+//            ),
+//            GradeSummaryModel(
+//                year: 2022,
+//                semester: "겨울학기",
+//                gpa: 1.92,
+//                earnedCredit: 4.0,
+//                semesterRank: 123,
+//                semesterStudentCount: 324,
+//                overallRank: 1,
+//                overallStudentCount: 161
+//            ),
+//            GradeSummaryModel(
+//                year: 2023,
+//                semester: "1 학기",
+//                gpa: 3.50,
+//                earnedCredit: 19.5,
+//                semesterRank: 11,
+//                semesterStudentCount: 342,
+//                overallRank: 545,
+//                overallStudentCount: 586
+//            )
+//        ])
 //    }
 //}
-
-final class TestSemesterListViewModel: BaseViewModel, SemesterListViewModel {
-    @Published var reportList = [GradeSummaryModel]()
-    @Published var isLoading: Bool = false
-    @Published var isOnSeasonalSemester: Bool = false
-    private let reportCardRepository = ReportCardRepository.shared
-
-    func getReportList() async -> Result<[GradeSummaryModel], RusaintError> {
-        return await getReportListFromRusaint()
-    }
-    func getReportListFromRusaint() async -> Result<[GradeSummaryModel], RusaintError> {
-        return .success([
-            GradeSummaryModel(
-                year: 2022,
-                semester: "1 학기",
-                gpa: 2.22,
-                earnedCredit: 13.5,
-                semesterRank: 12,
-                semesterStudentCount: 34,
-                overallRank: 100,
-                overallStudentCount: 566
-            ),
-            GradeSummaryModel(
-                year: 2022,
-                semester: "여름학기",
-                gpa: 3.22,
-                earnedCredit: 13.5,
-                semesterRank: 123,
-                semesterStudentCount: 111,
-                overallRank: 1,
-                overallStudentCount: 324
-            ),
-            GradeSummaryModel(
-                year: 2022,
-                semester: "2 학기",
-                gpa: 4.42,
-                earnedCredit: 4.0,
-                semesterRank: 123,
-                semesterStudentCount: 100,
-                overallRank: 11,
-                overallStudentCount: 324
-            ),
-            GradeSummaryModel(
-                year: 2022,
-                semester: "겨울학기",
-                gpa: 1.92,
-                earnedCredit: 4.0,
-                semesterRank: 123,
-                semesterStudentCount: 324,
-                overallRank: 1,
-                overallStudentCount: 161
-            ),
-            GradeSummaryModel(
-                year: 2023,
-                semester: "1 학기",
-                gpa: 3.50,
-                earnedCredit: 19.5,
-                semesterRank: 11,
-                semesterStudentCount: 342,
-                overallRank: 545,
-                overallStudentCount: 586
-            )
-        ])
-    }
-}
