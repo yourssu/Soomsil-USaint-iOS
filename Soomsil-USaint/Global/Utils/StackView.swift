03790fe16c54369a54c9865fde2516da34ff57bd
CJiu01 <jiwoo020801@gmail.com>
2024-12-21 19:12:53 +0900

    [#24] stackView.SemesterDetail 연관값 추가를 위한 StackViewType 열거형 수정

--- Soomsil-USaint/Global/Utils/StackView.swift
+++ Soomsil-USaint/Global/Utils/StackView.swift
@@ -7,12 +7,47 @@
 
 import Foundation
 
-enum StackViewType {
+enum StackViewType: Hashable {
     case SemesterList
-    case SemesterDetail
+    case SemesterDetail(GradeSummaryModel)
     case Setting
     case WebViewTerm
     case WebViewPrivacy
+
+    // MARK: - 연관값 추가하려면 아래 코드 필요
+    
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case .SemesterList:
+            hasher.combine("SemesterList")
+        case .SemesterDetail(let model):
+            hasher.combine("SemesterDetail")
+            hasher.combine(model)
+        case .Setting:
+            hasher.combine("Setting")
+        case .WebViewTerm:
+            hasher.combine("WebViewTerm")
+        case .WebViewPrivacy:
+            hasher.combine("WebViewPrivacy")
+        }
+    }
+
+    static func == (lhs: StackViewType, rhs: StackViewType) -> Bool {
+        switch (lhs, rhs) {
+        case (.SemesterList, .SemesterList):
+            return true
+        case (.SemesterDetail(let lhsModel), .SemesterDetail(let rhsModel)):
+            return lhsModel == rhsModel
+        case (.Setting, .Setting):
+            return true
+        case (.WebViewTerm, .WebViewTerm):
+            return true
+        case (.WebViewPrivacy, .WebViewPrivacy):
+            return true
+        default:
+            return false
+        }
+    }
 }
 
 struct StackView: Hashable {
