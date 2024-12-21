63d434ce8e8261de7c2eaf34861506c90fbb86a1
LeeJoEun-01 <joeun1005@naver.com>
2024-12-21 23:30:25 +0900

    [#27] SaintNexus 삭제

--- Soomsil-USaint/Global/Utils/Extension/SaintNexusModels+.swift
+++ Soomsil-USaint/Global/Utils/Extension/SaintNexusModels+.swift
@@ -6,20 +6,20 @@
 //  Copyright © 2024 Yourssu. All rights reserved.
 //
 
-import Foundation
-import SaintNexus
-
-public extension SNReportList {
-    func toDictionaries() -> [[String: String]] {
-        self.body.map { array in
-            Dictionary(uniqueKeysWithValues: zip(self.header, array))
-        }
-    }
-//    func toReportListModels() -> [GradeSummaryModel] {
-//        let list = self.body.map { array in
-//            let dict = Dictionary(uniqueKeysWithValues: zip(self.header, array))
-//            return GradeSummaryModel.init(dict)
+//import Foundation
+//import SaintNexus
+//
+//public extension SNReportList {
+//    func toDictionaries() -> [[String: String]] {
+//        self.body.map { array in
+//            Dictionary(uniqueKeysWithValues: zip(self.header, array))
 //        }
-//        return list.compactMap { $0 }
 //    }
-}
+////    func toReportListModels() -> [GradeSummaryModel] {
+////        let list = self.body.map { array in
+////            let dict = Dictionary(uniqueKeysWithValues: zip(self.header, array))
+////            return GradeSummaryModel.init(dict)
+////        }
+////        return list.compactMap { $0 }
+////    }
+//}
