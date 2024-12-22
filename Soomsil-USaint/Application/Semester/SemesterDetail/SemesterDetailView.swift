61df2d6bf776efbd547c28878c28e285415e53ea
CJiu01 <jiwoo020801@gmail.com>
2024-12-22 22:57:38 +0900

    [#33] detailView>GradeOverview 0일경우 "-"로 출력 변환

--- Soomsil-USaint/Application/Semester/SemesterDetail/SemesterDetailView.swift
+++ Soomsil-USaint/Application/Semester/SemesterDetail/SemesterDetailView.swift
@@ -49,7 +49,7 @@ struct SemesterDetailView<VM: SemesterDetailViewModel>: View {
                     VStack(alignment: .leading, spacing: Spacing.mainVStack) {
                         Text("\(String(semesterDetailViewModel.gradeSummary.year))년 \(semesterDetailViewModel.gradeSummary.semester)")
                             .font(YDSFont.subtitle2)
-                        if isShowSummary, semesterDetailViewModel.gradeSummary.gpa != 0 {
+                        if isShowSummary {
                             HStack(alignment: .lastTextBaseline) {
                                 Text(String(format: "%.2f", semesterDetailViewModel.gradeSummary.gpa))
                                     .font(YDSFont.display1)
