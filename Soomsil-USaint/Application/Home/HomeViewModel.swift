3854139b1ea0d857f2f64c33d6a99c407b00b3c4
LeeJoEun-01 <joeun1005@naver.com>
2025-01-06 16:11:25 +0900

    [#60] revert Home UI 수정

--- Soomsil-USaint/Application/Home/HomeViewModel.swift
+++ Soomsil-USaint/Application/Home/HomeViewModel.swift
@@ -6,23 +6,18 @@
 //
 
 import SwiftUI
-import Rusaint
 
 protocol HomeViewModel: ObservableObject {
     var person: PersonalInfo? { get set }
-    var fetchErrorMessage: String { get set }
     func isLogedIn() -> Bool
     func hasCachedUserInformation() -> Bool
     func syncCachedUserInformation()
     func getCachedUserInformation() -> PersonalInfo?
     func hasFeature(_ item: HomeItem) -> Bool
-    func loadCurrentSemesterData() async -> GradeSummaryModel
 }
 
 final class DefaultSaintHomeViewModel: HomeViewModel {
     @Published var person: PersonalInfo?
-    @Published var fetchErrorMessage: String = ""
-    private var session: USaintSession?
     private let homeRepository = HomeRepository.shared
 
     func isLogedIn() -> Bool {
@@ -54,99 +49,11 @@ final class DefaultSaintHomeViewModel: HomeViewModel {
             true
         }
     }
-    
-    // MARK: session
-    
-    @MainActor
-    private func setSession() async -> Result<Void, RusaintError> {
-        let userInfo = SemesterRepository.shared.getUserLoginInformation()
-        do {
-            self.session = try await USaintSessionBuilder().withPassword(id: userInfo[0], password: userInfo[1])
-            
-            guard self.session != nil else {
-                return .failure(.invalidClientError)
-            }
-            return .success(())
-            
-        } catch {
-            return .failure(.invalidClientError)
-        }
-    }
-    
-    // MARK: current semester
-    /**
-     2024년 2학기를 불러오는 함수입니다.
-     */
-    @MainActor
-    private func getCurrentSemesterGrade() async -> Result<GradeSummaryModel?, RusaintError> {
-        SemesterRepository.shared.deleteSemester(year: 2024, semester: "2 학기")
-        let userInfo = SemesterRepository.shared.getUserLoginInformation()
-        do {
-            self.session = try await USaintSessionBuilder().withPassword(id: userInfo[0], password: userInfo[1])
-            if self.session != nil {
-                do {
-                    let response = try await CourseGradesApplicationBuilder().build(session: self.session!).classes(courseType: .bachelor,
-                                                                                                                    year: 2024,
-                                                                                                                    semester: .two,
-                                                                                                                    includeDetails: false)
-                    if response.isEmpty {
-                        return .success(nil)
-                    }
-                    let currentClassesData = response.toLectureDetailModels()
-                    let currentSemester = GradeSummaryModel(year: 2024,
-                                                            semester: "2 학기",
-                                                            gpa: 0,
-                                                            earnedCredit: 0,
-                                                            semesterRank: 0,
-                                                            semesterStudentCount: 0,
-                                                            overallRank: 0,
-                                                            overallStudentCount: 0,
-                                                            lectures: currentClassesData)
-                    return .success(currentSemester)
-                } catch {
-                    return .failure(.applicationError)
-                }
-            }
-        } catch {
-            return .failure(.applicationError)
-        }
-        return .failure(.applicationError)
-    }
-    
-    private func saveCurrentSemesterToCoreData(_ currentSemester: GradeSummaryModel) {
-        SemesterRepository.shared.addSemester(currentSemester)
-    }
-    
-    /**
-     현재학기 GradeSummaryModel을 Rusaint에서 불러옵니다.
-     */
-    @MainActor
-    public func loadCurrentSemesterData() async -> GradeSummaryModel {
-        let currentSemesterGradeResponse = await getCurrentSemesterGrade()
-        switch currentSemesterGradeResponse {
-        case .success(let currentSemesterGrade):
-            if let currentSemesterGrade = currentSemesterGrade {
-                saveCurrentSemesterToCoreData(currentSemesterGrade)
-                return currentSemesterGrade
-            }
-            
-        case .failure(let error):
-            self.fetchErrorMessage = "\(error)"
-        }
-        return GradeSummaryModel(year: 0, semester: "0 학기")
-    }
-    
 }
 
 final class TestSaintMainHomeViewModel: HomeViewModel {
-    func loadCurrentSemesterData() async -> GradeSummaryModel {
-        return GradeSummaryModel(year: 2024, semester: "2 학기")
-    }
-    
-    var fetchErrorMessage: String = ""
-    
     @Published var person: PersonalInfo?
-    
+
     func isLogedIn() -> Bool {
         return self.person != nil
     }
