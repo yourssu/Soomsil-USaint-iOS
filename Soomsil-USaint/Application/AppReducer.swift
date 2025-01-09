54acb68fbd8e66b0aab2ba59a698ed6e7872b744
정지혁 <cars0106@icloud.com>
2025-01-09 13:30:56 +0900

    [#64] AppReducer 테스트 구현

--- Soomsil-USaint/Application/AppReducer.swift
+++ Soomsil-USaint/Application/AppReducer.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct AppReducer {
+    @ObservableState
+    enum State: Equatable {
+        case initial
+        case loggedOut
+        case loggedIn
+        
+        init() {
+            self = .initial
+        }
+    }
+    
+    enum Action {
+        case `init`
+    }
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .`init`:
+                state = .loggedOut
+                return .none
+            }
+        }
+    }
+}
