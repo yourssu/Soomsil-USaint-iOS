e5aa57bd80de03a76d4244ea400af58a90102e91
정지혁 <cars0106@icloud.com>
2025-01-12 23:17:07 +0900

    [#71] @Shared(.appStorage()) 테스트 Reducer 수정

--- Soomsil-USaint/Application/Feature/Home/Core/HomeReducer.swift
+++ Soomsil-USaint/Application/Feature/Home/Core/HomeReducer.swift
@@ -5,3 +5,32 @@
 //  Created by 이조은 on 1/6/25.
 //
 
+import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct HomeReducer {
+    @ObservableState
+    struct State {
+        @Shared(.appStorage("isFirstTest")) var isFirst = true
+    }
+    
+    enum Action {
+        case onAppear
+    }
+    
+    @Dependency(\.localNotificationClient) var localNotificationClient
+    
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                debugPrint("Before: \(state.isFirst)")
+                state.$isFirst.withLock { $0 = false }
+                debugPrint("After: \(state.isFirst)")
+                return .none
+            }
+        }
+    }
+}
