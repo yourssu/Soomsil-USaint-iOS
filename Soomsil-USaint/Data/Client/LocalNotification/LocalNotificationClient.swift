bdd6d864c77058f2e328ae054bd952bd58ee4a80
정지혁 <cars0106@icloud.com>
2025-01-12 22:32:49 +0900

    [#71] LocalNotificationClient 기능 구현
    - requestPushAuthorization, getPushAuthorizationStatus, setLecturePushNotification 구현 및 테스트

--- Soomsil-USaint/Data/Client/LocalNotification/LocalNotificationClient.swift
+++ Soomsil-USaint/Data/Client/LocalNotification/LocalNotificationClient.swift
@@ -10,7 +10,12 @@ import UIKit
 import ComposableArchitecture
 
 struct LocalNotificationClient {
+    // TODO: 논의가 필요한 부분 -> UserDefaults 대신 @Shared를 통해 permission과 isFirst를 사용하는 것이 어떤가요?
+    // isFirst는 Home에서만 사용해서 옮기는 것도 가능하지만, permission은 Home, Setting, backgroundTask에서 사용하니까 일단 전역으로 두는게 좋을까요?
     
+    var requestPushAuthorization: @Sendable () async throws -> Bool
+    var getPushAuthorizationStatus: @Sendable () async -> Bool
+    var setLecturePushNotification: @Sendable (String) async throws -> Void
 }
 
 extension DependencyValues {
@@ -21,9 +26,43 @@ extension DependencyValues {
 }
 
 extension LocalNotificationClient: DependencyKey {
-    static let liveValue: LocalNotificationClient = Self()
+    static let liveValue: LocalNotificationClient = Self(
+        requestPushAuthorization: {
+            return try await UNUserNotificationCenter.current()
+                .requestAuthorization(options: [.alert, .badge, .sound])
+        }, getPushAuthorizationStatus: {
+            let settings = await UNUserNotificationCenter.current().notificationSettings()
+            return (settings.authorizationStatus == .authorized) || (settings.authorizationStatus == .provisional)
+        }, setLecturePushNotification: { lectureTitle in
+            let content = UNMutableNotificationContent()
+            content.title = "숨쉴때 유세인트"
+            content.body = "[\(lectureTitle)] 과목의 성적이 공개되었어요."
+            content.sound = .default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: lectureTitle, content: content, trigger: trigger)
+            
+            try await UNUserNotificationCenter.current().add(request)
+        }
+    )
     
-    static let previewValue: LocalNotificationClient = Self()
+    static let previewValue: LocalNotificationClient = Self(
+        requestPushAuthorization: {
+            return true
+        }, getPushAuthorizationStatus: {
+            return true
+        }, setLecturePushNotification: { lectureTitle in
+            debugPrint(lectureTitle)
+        }
+    )
     
-    static let testValue: LocalNotificationClient = Self()
+    static let testValue: LocalNotificationClient = Self(
+        requestPushAuthorization: {
+            return true
+        }, getPushAuthorizationStatus: {
+            return true
+        }, setLecturePushNotification: { lectureTitle in
+            debugPrint(lectureTitle)
+        }
+    )
 }
