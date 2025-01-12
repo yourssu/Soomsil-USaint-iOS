72df5769fb831250078f8ae8a725ec1eab733f58
정지혁 <cars0106@icloud.com>
2025-01-12 21:05:28 +0900

    [#71] Empty LocalNotificationClient 구현

--- Soomsil-USaint/Data/Client/LocalNotification/LocalNotificationClient.swift
+++ Soomsil-USaint/Data/Client/LocalNotification/LocalNotificationClient.swift
@@ -5,3 +5,25 @@
 //  Created by 이조은 on 1/6/25.
 //
 
+import UIKit
+
+import ComposableArchitecture
+
+struct LocalNotificationClient {
+    
+}
+
+extension DependencyValues {
+    var localNotificationClient: LocalNotificationClient {
+        get { self[LocalNotificationClient.self] }
+        set { self[LocalNotificationClient.self] = newValue }
+    }
+}
+
+extension LocalNotificationClient: DependencyKey {
+    static let liveValue: LocalNotificationClient = Self()
+    
+    static let previewValue: LocalNotificationClient = Self()
+    
+    static let testValue: LocalNotificationClient = Self()
+}
