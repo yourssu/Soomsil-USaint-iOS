c84ca3ff01fa61619f1c2165076b02f1e44d7409
LeeJoEun-01 <joeun1005@naver.com>
2025-01-15 02:55:45 +0900

    [#69] StudentClient 테스트를 위한 SettingReducer 생성

--- Soomsil-USaint/Application/Feature/Setting/Core/SettingReducer.swift
+++ Soomsil-USaint/Application/Feature/Setting/Core/SettingReducer.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct SettingReducer {
+    @ObservableState
+    struct State {
+        var testStudentInfo: StudentInfo?
+    }
+
+    enum Action {
+        case onAppear
+        case getStudentInfo(StudentInfo)
+    }
+
+    @Dependency(\.studentClient) var studentClient
+
+    var body: some Reducer<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .run { send in
+                    try await studentClient.setStudentInfo()
+                    let info = try await studentClient.getStudentInfo()
+                    await send(.getStudentInfo(info))
+                } catch: { error, send in
+                    print("SettingReducer: \(error)")
+                }
+            case let .getStudentInfo(info):
+                state.testStudentInfo = info
+                return .none
+            }
+        }
+    }
+}
