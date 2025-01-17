874ccd63e0ba46b0bdd5af4565da020d7806daea
정지혁 <cars0106@icloud.com>
2025-01-17 15:06:53 +0900

    [#75] Empty LoginReducer 구현

--- Soomsil-USaint/Application/Feature/Login/Core/LoginReducer.swift
+++ Soomsil-USaint/Application/Feature/Login/Core/LoginReducer.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct LoginReducer {
+    @ObservableState
+    struct State: Equatable {
+        var isLoading = false
+        
+        var id = ""
+        var password = ""
+    }
+    
+    enum Action: BindableAction, Sendable {
+        case binding(BindingAction<State>)
+        case loginPressed
+        case loginResponse(Result<Void, Error>)
+    }
+    
+    var body: some Reducer<State, Action> {
+        BindingReducer()
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
