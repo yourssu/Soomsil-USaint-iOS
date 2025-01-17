c49853d2fe150c11e2334f62d1a4731a6ba26a68
CJiu01 <jiwoo020801@gmail.com>
2025-01-17 18:44:10 +0900

    [#70] GradeClient에서 studentClient 의존성 주입하여 session 생성

--- Soomsil-USaint/Data/Client/RuSaint/GradeClient.swift
+++ Soomsil-USaint/Data/Client/RuSaint/GradeClient.swift
@@ -23,7 +23,6 @@ struct GradeClient {
     var addGrades: (_ newSemester: GradeSummary) async throws -> Void
     var deleteAllSemesterGrades: () async throws -> Void
     var deleteGrades: (_ year: Int, _ semester: String) async throws -> Void
-
 }
 
 extension DependencyValues {
@@ -34,146 +33,150 @@ extension DependencyValues {
 }
 
 extension GradeClient: DependencyKey {
-    static var liveValue: GradeClient = GradeClient(
-        fetchAllSemesterGrades: {
-            let session = await fetchSession()
-            let response = try await CourseGradesApplicationBuilder()
-                .build(session: session)
-                .semesters(courseType: CourseType.bachelor)
-            return response
-        },
-        fetchGrades: { year, semester in
-            let session = await fetchSession()
-            let response = try await CourseGradesApplicationBuilder()
-                .build(session: session)
-                .classes(courseType: .bachelor,
-                         year: UInt32(year),
-                         semester: semester,
-                         includeDetails: false)
-            return response
-        },
-        getAllSemesterGrades: {
-            let context = coreDataStack.taskContext()
-            let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
-            
-            let fetchedEntity = try context.fetch(fetchRequest)
-            return fetchedEntity
-        },
-        getGrades: { year, semester in
-            let context = coreDataStack.taskContext()
-            let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
-            fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "year == %d", year),
-                NSPredicate(format: "semester == %@", semester)
-            ])
-            
-            if let fetchedEntity = try? context.fetch(fetchRequest).first {
+    static var liveValue: GradeClient = {
+        @Dependency(\.studentClient) var studentClient: StudentClient
+        
+        return GradeClient(
+            fetchAllSemesterGrades: {
+                let session = try await studentClient.createSaintSession()
+                let response = try await CourseGradesApplicationBuilder()
+                    .build(session: session)
+                    .semesters(courseType: CourseType.bachelor)
+                return response
+            },
+            fetchGrades: { year, semester in
+                let session = try await studentClient.createSaintSession()
+                let response = try await CourseGradesApplicationBuilder()
+                    .build(session: session)
+                    .classes(courseType: .bachelor,
+                             year: UInt32(year),
+                             semester: semester,
+                             includeDetails: false)
+                return response
+            },
+            getAllSemesterGrades: {
+                let context = coreDataStack.taskContext()
+                let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
+                
+                let fetchedEntity = try context.fetch(fetchRequest)
                 return fetchedEntity
-            } else {
-                return nil
-            }
-        },
-        // TODO: 해당 함수 호출 전, delete 먼저 수행해야 함.
-        updateAllSemesterGrades: { grades in
-            let context = coreDataStack.taskContext()
-            for grade in grades {
-                createSemester(year: grade.year,
-                               semester: grade.semester,
-                               gpa: grade.gpa,
-                               earnedCredit: grade.earnedCredit,
-                               semesterRank: grade.semesterRank,
-                               semesterStudentCount: grade.semesterStudentCount,
-                               overallRank: grade.overallRank,
-                               overallStudentCount: grade.overallStudentCount,
-                               lectures: grade.lectures ?? nil,
-                               in: context)
-            }
-            try context.save()
-        },
-        updateGrades: { year, semester, newLectures in
-            let context = coreDataStack.taskContext()
-            let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
-            
-            fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "year == %d", year),
-                NSPredicate(format: "semester == %@", semester)
-            ])
-            
-            if let semesterEntity = try context.fetch(fetchRequest).first {
-                semesterEntity.removeFromLectures(semesterEntity.lectures ?? [])
+            },
+            getGrades: { year, semester in
+                let context = coreDataStack.taskContext()
+                let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
+                fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "year == %d", year),
+                    NSPredicate(format: "semester == %@", semester)
+                ])
                 
-                let newLectureEntities = newLectures.map { lecture in
-                    let cdLecture = CDLecture(context: context)
-                    cdLecture.code = lecture.code
-                    cdLecture.title = lecture.title
-                    cdLecture.credit = Float(lecture.credit)
-                    cdLecture.score = lecture.score
-                    cdLecture.grade = lecture.grade.rawValue
-                    cdLecture.professorName = lecture.professorName
-                    return cdLecture
+                if let fetchedEntity = try? context.fetch(fetchRequest).first {
+                    return fetchedEntity
+                } else {
+                    return nil
                 }
+            },
+            // TODO: 해당 함수 호출 전, delete 먼저 수행해야 함.
+            updateAllSemesterGrades: { grades in
+                let context = coreDataStack.taskContext()
+                for grade in grades {
+                    createSemester(year: grade.year,
+                                   semester: grade.semester,
+                                   gpa: grade.gpa,
+                                   earnedCredit: grade.earnedCredit,
+                                   semesterRank: grade.semesterRank,
+                                   semesterStudentCount: grade.semesterStudentCount,
+                                   overallRank: grade.overallRank,
+                                   overallStudentCount: grade.overallStudentCount,
+                                   lectures: grade.lectures ?? nil,
+                                   in: context)
+                }
+                try context.save()
+            },
+            updateGrades: { year, semester, newLectures in
+                let context = coreDataStack.taskContext()
+                let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
                 
-                newLectureEntities.forEach { semesterEntity.addToLectures($0) }
-                context.performAndWait {
-                    do {
-                        try context.save()
-                    } catch {
-                        print("lectures 업데이트 실패: \(error.localizedDescription)")
+                fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "year == %d", year),
+                    NSPredicate(format: "semester == %@", semester)
+                ])
+                
+                if let semesterEntity = try context.fetch(fetchRequest).first {
+                    semesterEntity.removeFromLectures(semesterEntity.lectures ?? [])
+                    
+                    let newLectureEntities = newLectures.map { lecture in
+                        let cdLecture = CDLecture(context: context)
+                        cdLecture.code = lecture.code
+                        cdLecture.title = lecture.title
+                        cdLecture.credit = Float(lecture.credit)
+                        cdLecture.score = lecture.score
+                        cdLecture.grade = lecture.grade.rawValue
+                        cdLecture.professorName = lecture.professorName
+                        return cdLecture
+                    }
+                    
+                    newLectureEntities.forEach { semesterEntity.addToLectures($0) }
+                    context.performAndWait {
+                        do {
+                            try context.save()
+                        } catch {
+                            print("lectures 업데이트 실패: \(error.localizedDescription)")
+                        }
                     }
                 }
-            }
-        },
-        updateGPA: { year, semester, gpa in
-            let context = coreDataStack.taskContext()
-            let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
-            fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "year == %d", year),
-                NSPredicate(format: "semester == %@", semester)
-            ])
-            
-            if let fetchedEntity = try context.fetch(fetchRequest).first {
-                fetchedEntity.gpa = gpa
+            },
+            updateGPA: { year, semester, gpa in
+                let context = coreDataStack.taskContext()
+                let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
+                fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "year == %d", year),
+                    NSPredicate(format: "semester == %@", semester)
+                ])
+                
+                if let fetchedEntity = try context.fetch(fetchRequest).first {
+                    fetchedEntity.gpa = gpa
+                    try context.save()
+                }
+            },
+            addGrades: { newSemester in
+                let context = coreDataStack.taskContext()
+                
+                createSemester(year: newSemester.year,
+                               semester: newSemester.semester,
+                               gpa: newSemester.gpa,
+                               earnedCredit: newSemester.earnedCredit,
+                               semesterRank: newSemester.semesterRank,
+                               semesterStudentCount: newSemester.semesterStudentCount,
+                               overallRank: newSemester.overallRank,
+                               overallStudentCount: newSemester.overallStudentCount,
+                               lectures: newSemester.lectures ?? nil,
+                               in: context)
+                
+                try context.save()
+            },
+            deleteAllSemesterGrades: {
+                let context = coreDataStack.taskContext()
+                let deleteRequest = NSBatchDeleteRequest(fetchRequest: CDSemester.fetchRequest())
+                
+                try context.execute(deleteRequest)
+                try context.save()
+            },
+            deleteGrades: { year,semester in
+                let context = coreDataStack.taskContext()
+                let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
+                fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "year == %d", year),
+                    NSPredicate(format: "semester == %@", semester)
+                ])
+                
+                let fetchedEntity = try context.fetch(fetchRequest)
+                for entity in fetchedEntity {
+                    context.delete(entity)
+                }
                 try context.save()
             }
-        },
-        addGrades: { newSemester in
-            let context = coreDataStack.taskContext()
-            
-            createSemester(year: newSemester.year,
-                           semester: newSemester.semester,
-                           gpa: newSemester.gpa,
-                           earnedCredit: newSemester.earnedCredit,
-                           semesterRank: newSemester.semesterRank,
-                           semesterStudentCount: newSemester.semesterStudentCount,
-                           overallRank: newSemester.overallRank,
-                           overallStudentCount: newSemester.overallStudentCount,
-                           lectures: newSemester.lectures ?? nil,
-                           in: context)
-            
-            try context.save()
-        },
-        deleteAllSemesterGrades: {
-            let context = coreDataStack.taskContext()
-            let deleteRequest = NSBatchDeleteRequest(fetchRequest: CDSemester.fetchRequest())
-            
-            try context.execute(deleteRequest)
-            try context.save()
-        },
-        deleteGrades: { year,semester in
-            let context = coreDataStack.taskContext()
-            let fetchRequest: NSFetchRequest<CDSemester> = CDSemester.fetchRequest()
-            fetchRequest.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "year == %d", year),
-                NSPredicate(format: "semester == %@", semester)
-            ])
-            
-            let fetchedEntity = try context.fetch(fetchRequest)
-            for entity in fetchedEntity {
-                context.delete(entity)
-            }
-            try context.save()
-        }
-    )
+        )
+    }()
     
     static let previewValue = GradeClient {
         return
@@ -198,7 +201,6 @@ extension GradeClient: DependencyKey {
     }
     
     static let testValue: GradeClient = previewValue
-
 }
 
 private func createSemester(
@@ -222,7 +224,7 @@ private func createSemester(
     semesterEntity.semesterStudentCount = Int16(semesterStudentCount)
     semesterEntity.overallRank = Int16(overallRank)
     semesterEntity.overallStudentCount = Int16(overallStudentCount)
-
+    
     let lectureEntities = lectures?.compactMap { lecture -> CDLecture? in
         let cdLecture = CDLecture(context: context)
         cdLecture.code = lecture.code
