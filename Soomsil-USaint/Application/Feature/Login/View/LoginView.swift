b1f7c2895d53bea1de6c54a1b2070941873b2219
정지혁 <cars0106@icloud.com>
2025-01-24 16:10:56 +0900

    [#75] LoginForm 분리
    - 회의 때 논의 내용 반영

--- Soomsil-USaint/Application/Feature/Login/View/LoginView.swift
+++ Soomsil-USaint/Application/Feature/Login/View/LoginView.swift
@@ -29,7 +29,9 @@ struct LoginView: View {
         WithPerceptionTracking {
             VStack(spacing: 4) {
                 title
-                form
+                LoginForm(id: $store.id, password: $store.password) {
+                    store.send(.loginPressed)
+                }
                 Spacer()
             }
             .background {
@@ -46,45 +48,52 @@ struct LoginView: View {
             }
         }
     }
+    
+    struct LoginForm: View {
+        @Binding var id: String
+        @Binding var password: String
+        
+        let onLoginPressed: () -> Void
+        
+        var body: some View {
+            VStack(alignment: .leading, spacing: Dimension.VStack.spacing) {
+                Text("학번")
+                    .font(YDSFont.body1)
+                YDSSimpleTextField(text: $id)
+                
+                Text("유세인트 비밀번호")
+                    .font(YDSFont.body1)
+                SecureTextField(text: $password)
+                    .padding(.bottom, Dimension.largeSpace)
+                
+                Button {
+                    onLoginPressed()
+                } label: {
+                    Text("로그인")
+                        .foregroundStyle(YDSColor.buttonBright)
+                        .font(YDSFont.button4)
+                        .frame(maxWidth: .infinity, minHeight: Dimension.Button.minHeight)
+                        .background(YDSColor.buttonPoint, in: RoundedRectangle(cornerRadius: 5))
+                }
+                .buttonStyle(.plain)
+                
+                HStack {
+                    YDSIcon.warningcircleLine
+                        .renderingMode(.template)
+                    Text("숨쉴때 유세인트 서비스 이용을 위한 유세인트 학번 및 비밀번호는 사용자 기기에만 저장되며, 유어슈는 유세인트 서비스를 통하여 이용자의 정보를 일체 수집ㆍ저장하지 않습니다.")
+                        .font(.caption2)
+                }
+                .foregroundStyle(YDSColor.textPointed)
+            }
+            .padding(Dimension.padding)
+        }
+    }
 }
 
 private extension LoginView {
     var title: some View {
         Text("로그인")
             .font(YDSFont.subtitle2)
-    }
-    
-    var form: some View {
-        VStack(alignment: .leading, spacing: Dimension.VStack.spacing) {
-            Text("학번")
-                .font(YDSFont.body1)
-            YDSSimpleTextField(text: $store.id)
-            
-            Text("유세인트 비밀번호")
-                .font(YDSFont.body1)
-            SecureTextField(text: $store.password)
-                .padding(.bottom, Dimension.largeSpace)
-            
-            Button {
-                store.send(.loginPressed)
-            } label: {
-                Text("로그인")
-                    .foregroundStyle(YDSColor.buttonBright)
-                    .font(YDSFont.button4)
-                    .frame(maxWidth: .infinity, minHeight: Dimension.Button.minHeight)
-                    .background(YDSColor.buttonPoint, in: RoundedRectangle(cornerRadius: 5))
-            }
-            .buttonStyle(.plain)
-            
-            HStack {
-                YDSIcon.warningcircleLine
-                    .renderingMode(.template)
-                Text("숨쉴때 유세인트 서비스 이용을 위한 유세인트 학번 및 비밀번호는 사용자 기기에만 저장되며, 유어슈는 유세인트 서비스를 통하여 이용자의 정보를 일체 수집ㆍ저장하지 않습니다.")
-                    .font(.caption2)
-            }
-            .foregroundStyle(YDSColor.textPointed)
-        }
-        .padding(Dimension.padding)
     }
 }
 
