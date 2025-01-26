5c30f277340cfe04c6ac6b78cd325ad4bc51f7b1
CJiu01 <jiwoo020801@gmail.com>
2025-01-27 05:43:45 +0900

    [#78] togglePushAuthorization 관련 action 정의
    - togglePushAuthorization
    - pushAuthorizationResponse
    - configureSettingTapped
    - requestPushAuthorizationResponse

--- Soomsil-USaint/Application/Feature/Setting/Core/SettingReducer.swift
+++ Soomsil-USaint/Application/Feature/Setting/Core/SettingReducer.swift
@@ -5,27 +5,81 @@
 //  Created by 이조은 on 1/6/25.
 //
 
-import Foundation
+import UIKit
 
 import ComposableArchitecture
+import YDS_SwiftUI
 
 @Reducer
 struct SettingReducer {
     @ObservableState
     struct State {
-        var isNotificationEnabled: Bool = LocalNotificationManager.shared.getNotificationPermission()
+        @Shared(.appStorage("permission")) var permission = false
+        var alert: ActiveAlert? = nil
     }
     
-    enum Action {
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
         case logoutButtonTapped
-        case toggleNotificationButtonTapped
+        case togglePushAuthorization(Bool)
+        case pushAuthorizationResponse(Result<Bool, Error>)
+        case configureSettingTapped
+        case requestPushAuthorizationResponse(Result<Bool, Error>)
         case termsOfServiceButtonTapped
         case privacyPolicyButtonTapped
     }
     
+    @Dependency(\.localNotificationClient) var localNotificationClient
+    
     var body: some ReducerOf<Self> {
+        BindingReducer()
         Reduce { state, action in
-            return .none
+            switch action {
+            case .togglePushAuthorization(true):
+                return .run { send in
+                    await send(.pushAuthorizationResponse(Result {
+                        await localNotificationClient.getPushAuthorizationStatus()
+                    }))
+                }
+            case .togglePushAuthorization(false):
+                state.$permission.withLock { $0 = false }
+                return .none
+            case .pushAuthorizationResponse(.success(let granted)):
+                state.$permission.withLock { $0 = granted }
+                if !granted {
+                    state.alert = .permission
+                }
+                return .none
+            case .configureSettingTapped:
+                return .run { send in
+                    await send(.requestPushAuthorizationResponse(Result {
+                        return try await localNotificationClient.requestPushAuthorization()
+                    }))
+                }
+            case .requestPushAuthorizationResponse(.success(let granted)):
+                if !granted {
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        DispatchQueue.main.async {
+                            if UIApplication.shared.canOpenURL(url) {
+                                UIApplication.shared.open(url)
+                            }
+                        }
+                    }
+                }
+                return .none
+                        
+            case .logoutButtonTapped:
+                YDSToast("logoutButtonTapped 성공하였습니다.", haptic: .success)
+                return .none
+            case .termsOfServiceButtonTapped:
+                YDSToast("termsOfServiceButtonTapped 성공하였습니다.", haptic: .success)
+                return .none
+            case .privacyPolicyButtonTapped:
+                YDSToast("privacyPolicyButtonTapped 성공하였습니다.", haptic: .success)
+                return .none
+            default:
+                return .none
+            }
         }
     }
 }
