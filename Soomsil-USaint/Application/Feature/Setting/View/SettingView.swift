a78f5d0cff077d11680cd86a2c5336e678391da2
CJiu01 <jiwoo020801@gmail.com>
2025-01-27 05:55:28 +0900

    [#78] SettingView: send호출 enum 처리 및 alert 추가
    alert(legacy code)

--- Soomsil-USaint/Application/Feature/Setting/View/SettingView.swift
+++ Soomsil-USaint/Application/Feature/Setting/View/SettingView.swift
@@ -12,17 +12,66 @@ import YDS_SwiftUI
 
 struct SettingView: View {
     @Perception.Bindable var store: StoreOf<SettingReducer>
-    @State private var testIsNotificationEnabled = false
+    @State private var activeAlert: ActiveAlert?
     
     var body: some View {
-        VStack(spacing: 4) {
-            title
-            SettingList(isNotificationEnabled: $testIsNotificationEnabled)
+        WithPerceptionTracking {
+            VStack(spacing: 4) {
+                title
+                SettingList(isPushAuthorizationEnabled: $store.permission) { tappedItem in
+                    switch tappedItem {
+                    case .logout:
+                        store.send(.logoutButtonTapped)
+                    case .toggleAuthorization(let granted):
+                        store.send(.togglePushAuthorization(granted))
+                    case .termsOfService:
+                        store.send(.termsOfServiceButtonTapped)
+                    case .privacyPolicy:
+                        store.send(.privacyPolicyButtonTapped)
+                    }
+                }
+            }
+            .registerYDSToast()
+            .alert(item: $store.alert) { alertType in
+                switch alertType {
+                case .logout:
+                    return Alert(
+                        title: Text("로그아웃 하시겠습니까?"),
+                        message: nil,
+                        primaryButton: .default(
+                            Text("취소")
+                        ),
+                        secondaryButton: .destructive(
+                            Text("로그아웃"),
+                            action: {
+                                store.send(.logoutButtonTapped)
+                            }
+                        )
+                    )
+                case .permission:
+                    return Alert(
+                        title: Text("알림 설정"),
+                        message: Text("알림에 대한 권한 사용을 거부하였습니다. 기능 사용을 원하실 경우 설정 > 앱 > 숨쉴때 유세인트 > 알림 권한 허용을 해주세요."),
+                        primaryButton: .default(
+                            Text("취소")
+                        ),
+                        secondaryButton: .default(
+                            Text("설정"),
+                            action: {
+                                store.send(.configureSettingTapped)
+                            }
+                        )
+                    )
+                }
+            }
+            
         }
     }
     
     struct SettingList: View {
-        @Binding var isNotificationEnabled: Bool
+        @Binding var isPushAuthorizationEnabled: Bool
+        
+        let listItemTapped: (listItem) -> Void
         
         var body: some View {
             VStack(alignment: .leading) {
@@ -31,7 +80,9 @@ struct SettingView: View {
                     items: [
                         itemAction(
                             text: "로그아웃",
-                            action: {  }
+                            action: {
+                                listItemTapped(.logout)
+                            }
                         )
                     ]
                 )
@@ -46,12 +97,16 @@ struct SettingView: View {
                             .font(YDSFont.button3)
                             .foregroundColor(YDSColor.textSecondary)
                             .frame(maxWidth: .infinity, alignment: .leading)
-
-                        Toggle("", isOn: $isNotificationEnabled)
+                        
+                        Toggle("", isOn: $isPushAuthorizationEnabled)
                             .labelsHidden()
                             .padding(.horizontal, 20)
                             .padding(.vertical, 20)
                             .tint(YDSColor.buttonPoint)
+                            .onChange(of: isPushAuthorizationEnabled) { newValue in
+                                print("newValue: \(newValue)")
+                                listItemTapped(.toggleAuthorization(newValue))
+                            }
                     }
                     .padding(20)
                     .frame(height: 48)
@@ -61,18 +116,22 @@ struct SettingView: View {
                     items: [
                         itemAction(
                             text: "이용약관",
-                            action: {  }
+                            action: {
+                                listItemTapped(.termsOfService)
+                            }
                         ),
                         itemAction(
                             text: "개인정보 처리 방침",
-                            action: {  }
+                            action: {
+                                listItemTapped(.privacyPolicy)
+                            }
                         )
                     ]
                 )
                 Spacer()
             }
         }
-
+        
     }
 }
 
@@ -83,7 +142,28 @@ private extension SettingView {
     }
 }
 
+enum ActiveAlert: Identifiable {
+    case logout
+    case permission
+    
+    var id: String {
+        switch self {
+        case .logout:
+            return "logout"
+        case .permission:
+            return "permission"
+        }
+    }
+}
+
 // MARK: - List
+enum listItem {
+    case logout
+    case toggleAuthorization(Bool)
+    case termsOfService
+    case privacyPolicy
+}
+
 struct itemAction {
     let text: String
     let action: () -> Void
@@ -92,15 +172,15 @@ struct itemAction {
 struct ListRowView: View {
     let title: String
     let items: [itemAction]
-
+    
     @State private var pressedStates: [Bool]
-
+    
     init(title: String, items: [itemAction]) {
         self.title = title
         self.items = items
         self._pressedStates = State(initialValue: Array(repeating: false, count: items.count))
     }
-
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             Text(title)
@@ -108,7 +188,7 @@ struct ListRowView: View {
                 .foregroundColor(YDSColor.textSecondary)
                 .padding(20)
                 .frame(height: 48)
-
+            
             ForEach(items.indices, id: \.self) { index in
                 ItemModel(
                     text: items[index].text,
@@ -124,7 +204,7 @@ struct ItemModel: View {
     let text: String
     let action: () -> Void
     @Binding var isPressed: Bool
-
+    
     var body: some View {
         Text(text)
             .font(YDSFont.button3)
@@ -133,7 +213,6 @@ struct ItemModel: View {
             .frame(height: 48)
             .frame(maxWidth: .infinity, alignment: .leading)
             .background(isPressed ? Color(red: 0.95, green: 0.96, blue: 0.97) : Color.clear)
-            .contentShape(Rectangle())
             .gesture(
                 DragGesture(minimumDistance: 0)
                     .onChanged { _ in isPressed = true }
