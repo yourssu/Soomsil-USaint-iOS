efcbb7175e7d3e68acf48c1b9357d695bdf8cdb8
정지혁 <cars0106@icloud.com>
2025-02-18 00:55:15 +0900

    [#63] RemoteConfigClient 구현
    - getMinimumVersion 내에 active 및 fetch 구현

---
+++ Soomsil-USaint/Data/Client/Firebase/RemoteConfigClient.swift
@@ -0,0 +1,46 @@
+//
+//  RemoteConfigClient.swift
+//  Soomsil-USaint
+//
+//  Created by 정지혁 on 2/18/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+import FirebaseRemoteConfig
+
+@DependencyClient
+struct RemoteConfigClient {
+    private static let minimumVersionkey: String = "min_version_ios"
+    
+    var getMinimumVersion: @Sendable () async throws -> String
+}
+
+extension DependencyValues {
+    var remoteConfigClient: RemoteConfigClient {
+        get { self[RemoteConfigClient.self] }
+        set { self[RemoteConfigClient.self] = newValue }
+    }
+}
+
+extension RemoteConfigClient: DependencyKey {
+    static let liveValue: RemoteConfigClient = Self(
+        getMinimumVersion: {
+            let remoteConfig = RemoteConfig.remoteConfig()
+            try await remoteConfig.fetchAndActivate()
+            
+            let minimumVersion = remoteConfig[minimumVersionkey].stringValue
+            debugPrint(minimumVersion)
+            return minimumVersion
+        }
+    )
+    
+    static let previewValue: RemoteConfigClient = Self(
+        getMinimumVersion: {
+            return "1.0.0"
+        }
+    )
+    
+    static let testValue: RemoteConfigClient = previewValue
+}
