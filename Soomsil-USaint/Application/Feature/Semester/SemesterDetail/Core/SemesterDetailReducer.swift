d1752a1bbbf923c574db53da5a0f8843dcb1e186
CJiu01 <jiwoo020801@gmail.com>
2025-03-16 23:34:11 +0900

    [#90] Empty SemesterDetailReducer 구현

--- Soomsil-USaint/Application/Feature/Semester/SemesterDetail/Core/SemesterDetailReducer.swift
+++ Soomsil-USaint/Application/Feature/Semester/SemesterDetail/Core/SemesterDetailReducer.swift
@@ -5,3 +5,30 @@
 //  Created by 이조은 on 1/6/25.
 //
 
+import ComposableArchitecture
+
+@Reducer
+struct SemesterDetailReducer {
+    @ObservableState
+    struct State {
+        var gradeList: [LectureDetail]
+    }
+    
+    enum Action: BindableAction {
+        case binding(BindingAction<State>)
+        case onAppear
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        BindingReducer()
+        Reduce { state, action in
+            switch action {
+            case .onAppear:
+                return .none
+            case .binding(_):
+                return .none
+            }
+        }
+    }
+}
