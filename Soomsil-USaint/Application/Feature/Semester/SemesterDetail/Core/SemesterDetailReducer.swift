56635aa99fa36d3872bfe873e73151df375e9034
CJiu01 <jiwoo020801@gmail.com>
2025-03-21 16:57:05 +0900

    [#90] gradeClient로부터 학기정보 받아오는 action 추가

--- Soomsil-USaint/Application/Feature/Semester/SemesterDetail/Core/SemesterDetailReducer.swift
+++ Soomsil-USaint/Application/Feature/Semester/SemesterDetail/Core/SemesterDetailReducer.swift
@@ -6,29 +6,50 @@
 //
 
 import ComposableArchitecture
+import YDS_SwiftUI
 
 @Reducer
 struct SemesterDetailReducer {
     @ObservableState
     struct State {
-        var gradeList: [LectureDetail] 
+        var semesterList: [GradeSummary] = []
+        var isLoading: Bool = false
     }
     
     enum Action: BindableAction {
         case binding(BindingAction<State>)
         case onAppear
-        
+        case refresh
+        case semesterListResponse(Result<[GradeSummary], Error>)
     }
+    
+    @Dependency(\.gradeClient) var gradeClient
     
     var body: some ReducerOf<Self> {
         BindingReducer()
         Reduce { state, action in
             switch action {
             case .onAppear:
+                return .run { send in
+                    await send(.semesterListResponse(Result {
+                        try await gradeClient.getAllSemesterGrades()
+                    }))
+                }
+            case .refresh:
                 return .none
-            case .binding(_):
+            case .semesterListResponse(.success(let semesterList)):
+                state.semesterList = semesterList
+                state.isLoading = false
+                return .none
+            case .semesterListResponse(.failure(let error)):
+                debugPrint(error)
+                state.isLoading = false
+                YDSToast(String(describing: error), haptic: .failed)
+                return .none
+            default:
                 return .none
             }
+        
         }
     }
 }
