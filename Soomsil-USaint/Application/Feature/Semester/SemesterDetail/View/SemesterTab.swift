f86dc775a50b4a2c4cbf0d998b755afadda3eaef
CJiu01 <jiwoo020801@gmail.com>
2025-03-24 14:33:20 +0900

    [#90] SemesterTab title 프로퍼티 제거

--- Soomsil-USaint/Application/Feature/Semester/SemesterDetail/View/SemesterTab.swift
+++ Soomsil-USaint/Application/Feature/Semester/SemesterDetail/View/SemesterTab.swift
@@ -1,5 +1,5 @@
 //
-//  TabModel.swift
+//  SemesterTab.swift
 //  Soomsil-USaint
 //
 //  Created by 최지우 on 3/16/25.
@@ -7,10 +7,19 @@
 
 import SwiftUI
 
-struct TabModel: Identifiable, Hashable, Equatable {
-    var id: String { title }
-    
-    var title: String
+struct SemesterTab: Identifiable, Hashable, Equatable {
+    var id: String
     var size: CGSize = .zero
     var minX: CGFloat = .zero
+    
+    static func dummyTab() -> [SemesterTab] {
+        [
+           .init(id: "20년 1학기"),
+           .init(id: "21년 2학기2학기"),
+           .init(id: "21년 1학기"),
+           .init(id: "21년 2학기21년 2학기"),
+           .init(id: "22년 1학기"),
+           .init(id: "22년 2학기"),
+       ]
+    }
 }
