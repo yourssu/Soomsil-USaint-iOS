62e1ca6c766a18329c3e1da7412b2d74c8f70471
CJiu01 <jiwoo020801@gmail.com>
2025-03-26 00:41:03 +0900

    [#90] dummy data 삭제

--- Soomsil-USaint/Application/Feature/Semester/SemesterDetail/View/SemesterTab.swift
+++ Soomsil-USaint/Application/Feature/Semester/SemesterDetail/View/SemesterTab.swift
@@ -11,15 +11,4 @@ struct SemesterTab: Identifiable, Hashable, Equatable {
     var id: String
     var size: CGSize = .zero
     var minX: CGFloat = .zero
-    
-    static func dummyTab() -> [SemesterTab] {
-        [
-           .init(id: "20년 1학기"),
-           .init(id: "21년 2학기2학기"),
-           .init(id: "21년 1학기"),
-           .init(id: "21년 2학기21년 2학기"),
-           .init(id: "22년 1학기"),
-           .init(id: "22년 2학기"),
-       ]
-    }
 }
