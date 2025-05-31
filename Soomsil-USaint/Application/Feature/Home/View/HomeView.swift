7762452b31005f01f6b28841168153a7c1f37298
jysuhr <jun0.suhr@gmail.com>
2025-05-31 23:07:55 +0900

    [#117] ChapelInfo 새버전으로 그리기

--- Soomsil-USaint/Application/Feature/Home/View/HomeView.swift
+++ Soomsil-USaint/Application/Feature/Home/View/HomeView.swift
@@ -34,7 +34,7 @@ struct HomeView: View {
                         store.send(.semesterGradesPressed)
                     }
                     
-                    Chapel()
+                    ChapelInfo()
                     
                     Spacer()
                 }
